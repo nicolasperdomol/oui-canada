2a7ed0bdc8373f2b89a8ed2b309c4f9bc3274cce
niper <w.nicolasperdomo@gmail.com>
2023-05-22 18:42:53 -0400

    Fixing function error, JSX used instead

--- frontend/oui-canada/src/components/ContactUs.js
+++ frontend/oui-canada/src/components/ContactUs.js
@@ -1,128 +1,135 @@
-import { t } from "i18next";
-import Card from "react-bootstrap/Card"
-import Button from "react-bootstrap/Button";
+import { t } from 'i18next'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 // import ReCaptcha from "react-google-recaptcha"
-import EmailJS from "@emailjs/browser"
-import { useState } from "react";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import "../styles/css/ContactUs.css"
+import EmailJS from '@emailjs/browser'
+import { useState } from 'react'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import '../styles/css/ContactUs.css'
 
 const ContactUs = (props) => {
-    const [btnDisabled] = useState(true);
-    const [reCaptcha] = useState(null);
-    const mainTheme = props.mainTheme;
+  const [btnDisabled] = useState(true)
+  const [reCaptcha] = useState(null)
+  const mainTheme = props.mainTheme
 
-    // const handleReCaptcha = (token) => {
-    //     setReCaptcha(token)
-    //     setBtnDisabled(false);
-    // }
-    const toastNotify = (toastMsg) => {
-        toast.success(toastMsg, {
-        position: "top-center",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "light",
-        });
-    };
+  // const handleReCaptcha = (token) => {
+  //     setReCaptcha(token)
+  //     setBtnDisabled(false);
+  // }
+  const toastNotify = (toastMsg) => {
+    toast.success(toastMsg, {
+      position: 'top-center',
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: 'light'
+    })
+  }
 
-    const sendEmail = (e) => {
-        e.preventDefault();
-        const formParams = e.target;
-        if(formParams["firstname"].value.trim().length === 0
-            || formParams["lastname"].value.trim().length === 0
-            || formParams["email"].value.trim().length === 0){
-                console.log("Empty field found");
-                e.target.reset()
-                return;
-        }
-        const params = {
-            'firstname':formParams["firstname"].value.trim(),
-            'lastname':formParams["lastname"].value.trim(),
-            'email':formParams["email"].value.trim(),
-            'message':formParams["message"].value.trim(),
-            'date': new Date().toISOString().split('T')[0],
-            'g-recaptcha-response': reCaptcha
-        }
-        
-        EmailJS.send("oui-canada", "ouiCanadaForm", params, process.env.REACT_APP_EMAIL_KEY)
-            .then((result) => {
-                console.log(result.text)
-                toastNotify("Success")} 
-            ,(error)=>{
-                console.log(error)
-                toast.error("Ups, something went wrong")
-            });
-        e.target.reset();
+  const sendEmail = (e) => {
+    e.preventDefault()
+    const formParams = e.target
+    if (formParams.firstname.value.trim().length === 0 ||
+            formParams.lastname.value.trim().length === 0 ||
+            formParams.email.value.trim().length === 0) {
+      console.log('Empty field found')
+      e.target.reset()
+      return
+    }
+    const params = {
+      firstname: formParams.firstname.value.trim(),
+      lastname: formParams.lastname.value.trim(),
+      email: formParams.email.value.trim(),
+      message: formParams.message.value.trim(),
+      date: new Date().toISOString().split('T')[0],
+      'g-recaptcha-response': reCaptcha
     }
 
-    return(<div className="container-fluid">
-        <div id="contactUsRow" className="row justify-content-center">
-            <ToastContainer
-                position="top-center"
-                autoClose={5000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-                theme="light"
-            />
+    EmailJS.send('oui-canada', 'ouiCanadaForm', params, process.env.REACT_APP_EMAIL_KEY)
+      .then((result) => {
+        console.log(result.text)
+        toastNotify('Success')
+      }
+      , (error) => {
+        console.log(error)
+        toast.error('Ups, something went wrong')
+      })
+    e.target.reset()
+  }
 
-            <div className="col-4">
-                <h1 style={{padding:"16px 0 16px 0"}}>{t("contactUsTitle")}</h1>
-                {t("contactUsSubTitle")}
-                <ul id="unorderList">
-                    <li id="firstListItem"><b>{t("canada")}: </b>+1 (514) 571-0901</li>
-                    <li><b>Colombia: </b>+47 (111) 111-1111</li>
-                    <li><b>{t("email")}: </b>karen.lamilla@ouicanada.com</li>
-                    <li><b>{t("email")}: </b>karen.lamilla2@ouicanada.com</li>
-                </ul>
-            </div>
-            <div className="col-4">
-                <Card>
-                    <Card.Body className="p-5">
-                        <Card.Title><h2>{t("contactUsCardTitle")}</h2></Card.Title>
-                        <form className="container-fluid" onSubmit={sendEmail}>
-                            <div className="row">
-                                <div className="p-1"><div className="form-floating" id="floatingFirstName">
-                                    <input type="text" name="firstname" className="form-control" id="firstName" placeholder={t("firstName")}/>
-                                    <label htmlFor="floatingFirstName">{t("firstName")}</label>
-                                </div></div>
-                                <div className="p-1"><div className="form-floating" id="floatingLastName">
-                                    <input type="text" name="lastname" className="form-control" id="lastName" placeholder={t("lastName")}/>
-                                    <label htmlFor="floatingLastName">{t("lastName")}</label>
-                                </div></div>
-                                <div className="p-1">
-                                    <div className="form-floating" id="floatingEmail">
-                                        <input type="email" name="email" className="form-control" id="email" aria-describedby="emailHelp" placeholder={t("email")}/>
-                                        <label htmlFor="floatingEmail">{t("email")}</label>
-                                    </div>
-                                    <div id="emailHelp" className="form-text m-1">{t("emailSubtitle")}</div>
-                                    <div className="form-outline form-floating" id="floatingMessage">
-                                        <textarea className="form-control" name="message" id="textAreaExample" rows="4" placeholder="message"></textarea>
-                                        <label htmlFor="floatingMessage">{t("message")}</label>
-                                    </div>
-                                    {/* <ReCaptcha 
+  return (
+    <div className='container-fluid'>
+      <div id='contactUsRow' className='row justify-content-center'>
+        <ToastContainer
+          position='top-center'
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme='light'
+        />
+
+        <div className='col-4'>
+          <h1 style={{ padding: '16px 0 16px 0' }}>{t('contactUsTitle')}</h1>
+          {t('contactUsSubTitle')}
+          <ul id='unorderList'>
+            <li id='firstListItem'><b>{t('canada')}: </b>+1 (514) 571-0901</li>
+            <li><b>Colombia: </b>+47 (111) 111-1111</li>
+            <li><b>{t('email')}: </b>karen.lamilla@ouicanada.com</li>
+            <li><b>{t('email')}: </b>karen.lamilla2@ouicanada.com</li>
+          </ul>
+        </div>
+        <div className='col-4'>
+          <Card>
+            <Card.Body className='p-5'>
+              <Card.Title><h2>{t('contactUsCardTitle')}</h2></Card.Title>
+              <form className='container-fluid' onSubmit={sendEmail}>
+                <div className='row'>
+                  <div className='p-1'>
+                    <div className='form-floating' id='floatingFirstName'>
+                      <input type='text' name='firstname' className='form-control' id='firstName' placeholder={t('firstName')} />
+                      <label htmlFor='floatingFirstName'>{t('firstName')}</label>
+                    </div>
+                  </div>
+                  <div className='p-1'>
+                    <div className='form-floating' id='floatingLastName'>
+                      <input type='text' name='lastname' className='form-control' id='lastName' placeholder={t('lastName')} />
+                      <label htmlFor='floatingLastName'>{t('lastName')}</label>
+                    </div>
+                  </div>
+                  <div className='p-1'>
+                    <div className='form-floating' id='floatingEmail'>
+                      <input type='email' name='email' className='form-control' id='email' aria-describedby='emailHelp' placeholder={t('email')} />
+                      <label htmlFor='floatingEmail'>{t('email')}</label>
+                    </div>
+                    <div id='emailHelp' className='form-text m-1'>{t('emailSubtitle')}</div>
+                    <div className='form-outline form-floating' id='floatingMessage'>
+                      <textarea className='form-control' name='message' id='textAreaExample' rows='4' placeholder='message' />
+                      <label htmlFor='floatingMessage'>{t('message')}</label>
+                    </div>
+                    {/* <ReCaptcha
                                         className="recatcha"
                                         sitekey= {process.env.REACT_APP_SITE_KEY}
                                         onChange={(token)=>{handleReCaptcha(token)}}
                                     /> */}
-                                </div>
-                                <Button type="submit" id="contactUsFormBtn" className={`mt-4 ${mainTheme}_contactUsFormBtn`} disabled={btnDisabled}>Send</Button>
-                            </div>
-                        </form>
-                    </Card.Body>
-                </Card>
-            </div>
+                  </div>
+                  <Button type='submit' id='contactUsFormBtn' className={`mt-4 ${mainTheme}_contactUsFormBtn`} disabled={btnDisabled}>Send</Button>
+                </div>
+              </form>
+            </Card.Body>
+          </Card>
         </div>
-    </div>)
+      </div>
+    </div>
+  )
 }
 
-export default ContactUs;
+export default ContactUs
--- frontend/oui-canada/src/components/Hero.js
+++ frontend/oui-canada/src/components/Hero.js
@@ -1,61 +1,64 @@
-import { useTranslation } from "react-i18next";
-import {Fade} from "react-reveal";
-import blueGirl from "../img/blue-girl.png";
-import greyGirl from "../img/grey-girl.png";
-import redGirl from "../img/red-girl.png";
-import yellowGirl from "../img/yellow-girl.png";
-import {useRef, useEffect} from 'react'
-import "../styles/css/Hero.css";
-import Typed from "typed.js";
-import i18next from "i18next";
+import { useTranslation } from 'react-i18next'
+import { Fade } from 'react-reveal'
+import blueGirl from '../img/blue-girl.png'
+import greyGirl from '../img/grey-girl.png'
+import redGirl from '../img/red-girl.png'
+import yellowGirl from '../img/yellow-girl.png'
+import { useRef, useEffect } from 'react'
+import '../styles/css/Hero.css'
+import Typed from 'typed.js'
+import i18next from 'i18next'
 
 const Hero = (props) => {
-    const { t } = useTranslation();
-    const autoTyped = useRef(null);
-    const mainTheme = props.mainTheme;
-    const girlImage = (themes, mainTheme) => {
-        switch (mainTheme) {
-            case themes[0] : return blueGirl;
-            case themes[1] : return greyGirl;
-            case themes[2] : return redGirl;
-            default: return yellowGirl;
-        }
+  const { t } = useTranslation()
+  const autoTyped = useRef(null)
+  const mainTheme = props.mainTheme
+  const girlImage = (themes, mainTheme) => {
+    switch (mainTheme) {
+      case themes[0] : return blueGirl
+      case themes[1] : return greyGirl
+      case themes[2] : return redGirl
+      default: return yellowGirl
     }
+  }
 
-    useEffect(()=>{
-    const strings = i18next.language === "es" ? ["Viaja", "Estudia", "Trabaja", "Explora"] 
-        : ["Travel", "Study", "Work", "Explore"];
+  useEffect(() => {
+    const strings = i18next.language === 'es'
+      ? ['Viaja', 'Estudia', 'Trabaja', 'Explora']
+      : ['Travel', 'Study', 'Work', 'Explore']
 
-        const typed = new Typed(autoTyped.current, {
-            strings:strings,
-            startDelay:800,
-            typeSpeed:150,
-            backDelay:600,
-            backSpeed:250,
-            smartBackspace:false,
-            showCursor:true,
-            loop:true,
-        });
+    const typed = new Typed(autoTyped.current, {
+      strings,
+      startDelay: 800,
+      typeSpeed: 150,
+      backDelay: 600,
+      backSpeed: 250,
+      smartBackspace: false,
+      showCursor: true,
+      loop: true
+    })
 
-        return () => {
-        typed.destroy();
-        };
-    }, [])
+    return () => {
+      typed.destroy()
+    }
+  }, [])
 
-    return(<div className="container-fluid">
-        <div className="row heroMainRow">
-            <div className="col-4 heroCol offset-2 d-flex align-items-center">
-                <div className="solidBoxContainer"><div id={`${mainTheme}_heroTitleBox`}></div></div>
-                
-                <h1 id="heroTitle">{t("heroTitle")}<br/><span id="auto-typed" className={props.mainTheme + "_header"} ref={autoTyped}/></h1>
-            </div>
-            <div className="col-4 heroCol d-flex justify-content-center">
-                <Fade left>
-                    <img className="studyingGirl" src={girlImage(props.themes, props.mainTheme)} alt="Girl studying"/>
-                </Fade>
-            </div>
+  return (
+    <div className='container-fluid'>
+      <div className='row heroMainRow'>
+        <div className='col-4 heroCol offset-2 d-flex align-items-center'>
+          <div className='solidBoxContainer'><div id={`${mainTheme}_heroTitleBox`} /></div>
+
+          <h1 id='heroTitle'>{t('heroTitle')}<br /><span id='auto-typed' className={props.mainTheme + '_header'} ref={autoTyped} /></h1>
         </div>
-    </div>)
+        <div className='col-4 heroCol d-flex justify-content-center'>
+          <Fade left>
+            <img className='studyingGirl' src={girlImage(props.themes, props.mainTheme)} alt='Girl studying' />
+          </Fade>
+        </div>
+      </div>
+    </div>
+  )
 }
 
-export default Hero;
+export default Hero
